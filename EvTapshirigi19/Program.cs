88a9a4d2423e5318a5389fc5b9dc636cdd0fc6e7
kenanxasiyevv <kananakh@code.edu.az>
2023-01-04 02:58:14 +0400

    Add project files.

---
+++ EvTapshirigi19/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EvTapshirigi19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin uzunlugunu daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+         
+            int sum = 0;
+       
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0 ; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                        
+            }          
+                int edediorta = sum / arr.Length;
+                Console.WriteLine(edediorta);
+               
+        }
+    }
+}
